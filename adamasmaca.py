86f4edd9e7deff1ca53e6da55dfd7e7e7449e48c
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-05 13:41:04 +0300

    Update adamasmaca.py

--- adamasmaca.py
+++ adamasmaca.py
@@ -1,127 +1,59 @@
-import telethon
-import asyncio
+import telebot
 
-class adamasmaca:
-    def __init__(self, client, chat_id, word):
-        self.client = client
-        self.chat_id = chat_id
-        self.word = word
-        self.lives = 6
-        self.guessed = set()
-        self.game_over = False
+# Create a new bot with your API token
+bot = telebot.TeleBot("5861916928:AAF1szw5vhSWcaGksYeO2m9bS4FENSE6W9M")
 
-    async def play(self):
-        await self.client.send_message(self.chat_id, f"Adam asmaca oyununa hoşgeldiniz! {len(self.word)} harfli bir kelime söyleyin.")
-        await self.show_word()
-        while not self.game_over:
-            guess = await self.get_input()
-            if guess == self.word:
-                await self.client.send_message(self.chat_id, "Tebrikler, doğru bildiniz!")
-                self.game_over = True
-            elif guess in self.guessed:
-                await self.client.send_message(self.chat_id, f"{guess} zaten tahmin edildi.")
-            else:
-                self.guessed.add(guess)
-                if guess not in self.word:
-                    self.lives -= 1
-                    await self.show_adamasmaca()
-                    if self.lives == 0:
-                        await self.client.send_message(self.chat_id, f"Kaybettiniz! Kelime {self.word} idi.")
-                        self.game_over = True
-                    else:
-                        await self.client.send_message(self.chat_id, f"{guess} yanlış tahmin. {self.lives} canınız kaldı.")
-                else:
-                    await self.show_word()
-                    if set(self.word) == self.guessed:
-                        await self.client.send_message(self.chat_id, "Tebrikler, doğru bildiniz!")
-                        self.game_over = True
+# Define the game and its variables
+word = "vefa","cengo","mamaklı","ışık"
+guesses = []
+max_guesses = 6
 
-    async def get_input(self):
-        message = await self.client.send_message(self.chat_id, "Bir harf veya kelime tahmin edin.\n"
-                                                             "Her yanlış tahminde adamın bir bölümü çizilecektir.")
-        guess = (await self.client.get_messages(self.chat_id, limit=1))[0].message.strip().lower()
-        while not guess.isalpha() or len(guess) > 1 and len(guess) != len(self.word):
-            await self.client.send_message(self.chat_id, "Geçersiz tahmin. Lütfen bir harf veya kelime girin.")
-            guess = (await self.client.get_messages(self.chat_id, limit=1))[0].message.strip().lower()
-        return guess
+# Define the hangman drawing
+hangman_drawings = [
+    "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="
+]
 
-    async def show_word(self):
-        display_word = "".join([c if c in self.guessed else "_" for c in self.word])
-        await self.client.send_message(self.chat_id, display_word)
+# Define the game start command
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to Hangman! Type /play to start a new game.")
 
-    async def show_adamasmaca(self):
-        stages = [  
-            """
-                 --------
-                 |      |
-                 |      O
-                 |
-                 |
-                 |
-                 -
-            """,
-            """
-                 --------
-                 |      |
-                 |      O
-                 |      |
-                 |      |
-                 |
-                 -
-            """,
-            """
-                 --------
-                 |      |
-                 |      O
-                 |     \|
-                 |      |
-                 |
-                 -
-            """,
-            """
-                 --------
-                 |      |
-                 |      O
-                 |     \|/
-                 |      |
-                 |
-                 -
-            """,
-            """
-                 --------
-                 |      |
-                 |      O
-                 |     \|/
-                 |      |
-                 |     /
-                 -
-            """,
-            """
-                 --------
-                 |      |
-                 |      O
-                 |     \|/
-                 |      |
-                 |     / \\
-                 -
-            """
-        ]
-        await self.client.send_message(self.chat_id, stages[self.lives])
-        
-api_id = '25989627'
-api_hash = 'dff2250c7620fef64cd17e4355432d82'
-bot_token = '6022154020:AAEekctwuqrK8ZchRxW4_CPVEn-srDGgEYo'
+# Define the game play command
+@bot.message_handler(commands=['play'])
+def play_game(message):
+    global word, guesses
+    word = "python"
+    guesses = []
+    bot.reply_to(message, "Let's play Hangman! I'm thinking of a {}-letter word. Guess a letter by typing it in the chat.".format(len(word)))
 
-client = telethon.TelegramClient('adamasmamaca', api_id, api_hash).start(bot_token=bot_token)
+# Define the game guess handler
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    global word, guesses, max_guesses
+    if len(guesses) >= max_guesses:
+        bot.reply_to(message, "Sorry, you've run out of guesses! The word was {}.".format(word))
+        return
+    guess = message.text.lower()
+    if guess in guesses:
+        bot.reply_to(message, "You already guessed that letter! Guess again.")
+        return
+    guesses.append(guess)
+    if guess in word:
+        if set(word) == set(guesses):
+            bot.reply_to(message, "Congratulations, you've won! The word was {}.".format(word))
+            return
+        else:
+            masked_word = "".join([letter if letter in guesses else "_" for letter in word])
+            bot.reply_to(message, "Good guess! The word so far is:\n{}\n{}".format(masked_word, hangman_drawings[len(guesses)-1]))
+    else:
+        bot.reply_to(message, "Sorry, that letter is not in the word. Guess again.")
+        bot.reply_to(message, "{}\n{}".format(" ".join(guesses), hangman_drawings[len(guesses)-1]))
 
-
-@client.on(telethon.events.NewMessage(pattern='/adamas'))
-async def adamas(event):
-    game = adamasmaca(client, event.chat_id, "telethon")
-    await game.play()
-
-async def main():
-    await client.start()
-    await client.run_until_disconnected()
-
-asyncio.run(main())
+# Start the bot
+bot.polling()
